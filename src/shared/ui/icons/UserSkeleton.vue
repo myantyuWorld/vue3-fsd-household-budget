d9ee5cb4daac124a9b414b9626d1dbea844e7154
myantyuWorld <nepinepi984@gmail.com>
2025-04-15 19:30:38 +0900

    modify default layout

---
+++ src/shared/ui/icons/UserSkeleton.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex items-center gap-4">
+    <div class="w-10 h-10 rounded-full bg-gray-200 animate-pulse"></div>
+    <div class="h-4 w-20 bg-gray-200 rounded animate-pulse"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+// コンポーネントのロジックは不要
+</script> 
