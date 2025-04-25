5ac70f8639df2a28fd8c0479d550aa4ae2765eba
yuichi.oba <yuichi.oba@sorich.jp>
2025-04-26 08:29:46 +0900

    fix: login api url

--- src/pages/login/hooks/useInteraction.ts
+++ src/pages/login/hooks/useInteraction.ts
@@ -1,22 +1,21 @@
 import { ref } from 'vue'
 
 export const useInteraction = () => {
-    const isLoading = ref(false)
+  const isLoading = ref(false)
 
-    const onClickLogin = async () => {
-        isLoading.value = true
-        try {
-            // TODO : 環境変数化する
-            location.href = 'http://localhost:3000/line/login'
-        } catch (error) {
-            console.error('Login failed:', error)
-        } finally {
-            isLoading.value = false
-        }
+  const onClickLogin = async () => {
+    isLoading.value = true
+    try {
+      location.href = `${import.meta.env.VITE_API_BASE_URL}/line/login`
+    } catch (error) {
+      console.error('Login failed:', error)
+    } finally {
+      isLoading.value = false
     }
+  }
 
-    return {
-        isLoading,
-        onClickLogin
-    }
+  return {
+    isLoading,
+    onClickLogin,
+  }
 }
